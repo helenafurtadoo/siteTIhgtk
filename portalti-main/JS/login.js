16a4f5fd984c24cf5ae040ae36d3a07048f70582
richardalvescarmo-code <richard.alvescarmo@gmail.com>
2026-07-13 09:38:46 -0300

    Update login.js

--- portalti-main/JS/login.js
+++ portalti-main/JS/login.js
@@ -1,1 +1,17 @@
+function login(){
 
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+
+    if(usuario === "admin" && senha === "123456"){
+
+        window.location.href = "index.html";
+
+    }else{
+
+        alert("Usuário ou senha incorretos");
+
+    }
+
+}
