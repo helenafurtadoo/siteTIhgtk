263448c0b66cd05977155342b69dfcb004f43fea
Helena Furtado <helenafurtado25@gmail.com>
2026-07-21 11:56:11 -0300

    Altera login.js

--- portalti-main/JS/login.js
+++ portalti-main/JS/login.js
@@ -1,26 +1,28 @@
 function login(){
-
+ 
     let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
-
-
+ 
+ 
     if(usuario === "admin" && senha === "123456"){
-
+ 
         sessionStorage.setItem("logado", "sim");
-
-        window.location.href = "index.html";
-
+ 
+        window.location.href = "portal.html";
+ 
     }else{
-
+ 
         alert("Usuário ou senha incorretos");
-
+ 
     }
-
+ 
 }
 function sair(){
-
+ 
     sessionStorage.removeItem("logado");
-
-    window.location.href = "login.html";
-
+ 
+    window.location.href = "index.html";
+ 
 }
+ 
+ 
